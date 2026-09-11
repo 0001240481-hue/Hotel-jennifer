bfb313330c229c08a378b94e237fe371be9aeeb7
Kaik <0001231422@senaimgaluno.com.br>
2026-09-10 22:00:58 -0300

    arquivos iniciais do projeto

--- autenticar.php
+++ autenticar.php
@@ -1,37 +1,22 @@
 <?php
-
 require_once "conexao.php";
 
-$email=$_POST['email'];
-$senha=$_POST['senha'];
+$email = $_POST['email'] ?? '';
+$senha = $_POST['senha'] ?? '';
 
-$sql = "SELECT * FROM clientes WHERE email ='$email'and senha=;
-'$senha'";
-$resultado = mysqli_query($conexao,$sql);
+$sql = "SELECT * FROM clientes WHERE email = '$email' and senha = '$senha'";
 
-if (mysqli_num_rows ($resultado) > 0) {
-
-$cliente =
-mysqli_fetch_assoc($resultado);
+$resultado = mysqli_query(
+    $conexao,
+    $sql
+);
 
 
-if($senha == $cliente ["senha"]){
-
-session_start();
-$_SESSION["email"] = $email;
-
-header("location:minhas_reservas.php");
-
-}else {
-    echo"senha incorreta"
-    echo"<a
-    href='login.html'> voltar para o login</a>";
-}
-
-
-
-}else {
-    echo"E_mail não encontrado!";
-    echo"<a href='login.html'>Voltar para o login</a>";
+if (mysqli_num_rows($resultado) > 0){
+    header("Location: minhas_reservas.php");
+    exit();
+}else{
+    header("Location: login.html");
+    exit();
 }
 ?>
